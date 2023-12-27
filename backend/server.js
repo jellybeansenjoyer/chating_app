2ef6574e509ca3128a289c679ef8778c4bbee768
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2023-12-27 10:41:32 +0530

    project initialised using npm init, server ready and running, done with gitignore

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const dotenv = require('dotenv');
+const app = express();
+dotenv.config();
+app.get('/', (req, res) => {
+    res.send("API is Running");
+});
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, console.log("Server started at PORT 5000"))
