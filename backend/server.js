849011a0c5d25719b845a0aa0a55ae17481b8dba
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2023-12-27 10:51:59 +0530

    frontend initialised

--- backend/server.js
+++ backend/server.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 4000
 
-app.listen(PORT, console.log("Server started at PORT 5000"))
+app.listen(PORT, console.log(`Server started at PORT ${PORT}`))
