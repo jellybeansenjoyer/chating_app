b6137192d90bc586b39209d22e511c0fbed14149
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2023-12-31 02:48:39 +0530

    adding the fields for signup

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import { FormControl,FormLabel,Input } from '@chakra-ui/react'
+import { VStack } from '@chakra-ui/layout'
+import React,{useState} from 'react'
 
 const Signup = () => {
+    const [name,setName] = useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [confirmPassword,setConfirmPassword] = useState('');
+    const [pic,setPic] = useState('');
   return (
-    <div>
-        Signup
-    </div>
+    
+
+    <VStack spacing = '5px'>
+        <FormControl>
+            <FormLabel>
+                <Input placeholder="Enter your name" 
+                onChange={(e)=>{
+                    setName(e.target.value)
+                }} />
+            </FormLabel>
+        </FormControl>
+    </VStack>
   )
 }
 
