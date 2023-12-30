9a90cbb49e92befc8b1b40408fde3df1551c0245
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2023-12-31 01:53:46 +0530

    key element inclusion

---
+++ frontend/src/pages/Chatpage.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+const Chatpage = () => {
+    const [chats,setChats] = useState(['']);
+    const fetchChats = async () => {
+        const {data} = await axios.get("/api/chat")
+        setChats(data);
+        console.log(data)
+    }
+    useEffect(()=>{
+        fetchChats();
+    },[])
+    
+    return (
+    <div>{chats.map(chat=><div key={chat._id}>{chat}</div>)}
+    </div>
+  )
+}
+
+export default Chatpage
