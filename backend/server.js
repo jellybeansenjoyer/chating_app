f37977a867f43256df030291ac7ba0337eb1bbef
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2023-12-31 01:52:30 +0530

    first communication

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ const dotenv = require('dotenv');
 const app = express();
 dotenv.config();
 app.get('/', (req, res) => {
-    res.send("API is Running");
+    res.status(200).send("API is Running");
 });
-
+app.get('/api/chat',(req,res)=>{
+    res.status(200).send(["Raghav","Kashyap"]);
+})
 const PORT = process.env.PORT || 4000
 
 app.listen(PORT, console.log(`Server started at PORT ${PORT}`))
