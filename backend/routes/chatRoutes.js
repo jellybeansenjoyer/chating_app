534d7d02835c2b8743d4a23a3d73991cbc4110d5
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2024-01-06 12:50:46 +0530

    put for rename

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {accessChat,fetchChats,createGroupChat} = require('../controller/chatController')
+const {accessChat,fetchChats,createGroupChat,renameGroup} = require('../controller/chatController')
 const { protect } = require('../middleware/authMiddleware')
 router.route('/').post(protect,accessChat);
 router.route('/').get(protect,fetchChats);
 router.route('/group').post(protect,createGroupChat)
-// router.route('/rename').put(protect,renameGroup);
+router.route('/rename').put(protect,renameGroup);
 // router.route('/groupRemove').put(protect,removeFromGroup);
 // router.route('/groupAdd').put(protect,addToGroup)
 
