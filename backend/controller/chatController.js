e05d79a459e478d3949c997cf1b366f2c2fd890b
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2024-01-06 12:04:22 +0530

    createGroup successful

--- backend/controller/chatController.js
+++ backend/controller/chatController.js
@@ -57,5 +57,35 @@ const fetchChats = asyncHandler(async(req,res)=>{
         res.status(400)
         throw new Error(err.message);
     }
-})
-module.exports = {accessChat,fetchChats}
+});
+
+const createGroupChat = asyncHandler(async(req,res)=>{
+    if(!req.body.users || !req.body.name){
+        return res.status(400).send({message:"Please Fill all the Fields"});
+    }
+
+    var users = JSON.parse(req.body.users);
+    if (users.length < 2){
+        return res.status(400).send("More than 2 users are required to form a group chat");
+    }
+
+    users.push(req.user);
+
+    try {
+        const groupChat = await Chat.create({
+            chatName:req.body.name,
+            users:users,
+            isGroupChat:true,
+            groupAdmin:req.user,
+        })
+
+        const fullGroup = await Chat.findOne({_id:groupChat._id})
+            .populate("users","-password")
+            .populate("groupAdmin","-password")
+        return res.status(200).json(fullGroup);
+    } catch (error) {
+        res.status(400);
+        throw new Error(error.message);
+    }
+});
+module.exports = {accessChat,fetchChats,createGroupChat}
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {accessChat,fetchChats} = require('../controller/chatController')
+const {accessChat,fetchChats,createGroupChat} = require('../controller/chatController')
 const { protect } = require('../middleware/authMiddleware')
 router.route('/').post(protect,accessChat);
 router.route('/').get(protect,fetchChats);
-// router.route('/group').post(protect,createGroupChat)
+router.route('/group').post(protect,createGroupChat)
 // router.route('/rename').put(protect,renameGroup);
 // router.route('/groupRemove').put(protect,removeFromGroup);
 // router.route('/groupAdd').put(protect,addToGroup)
