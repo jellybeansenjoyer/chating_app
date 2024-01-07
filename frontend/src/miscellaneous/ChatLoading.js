30f30225331369d892afef8df5f063760ff3bc80
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2024-01-07 15:20:09 +0530

    loading

---
+++ frontend/src/miscellaneous/ChatLoading.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Skeleton} from '@chakra-ui/skeleton';
+import {Stack} from '@chakra-ui/layout';
+const ChatLoading = () => {
+  return (
+    <Stack>
+        <Skeleton height="45px" />
+        <Skeleton height="45px" />
+        <Skeleton height="45px" />
+        <Skeleton height="45px" />
+        <Skeleton height="45px" />
+        <Skeleton height="45px" />
+        <Skeleton height="45px" />
+        <Skeleton height="45px" />
+        <Skeleton height="45px" />
+        <Skeleton height="45px" />
+        <Skeleton height="45px" />
+    </Stack>
+  )
+}
+
+export default ChatLoading;
--- frontend/src/miscellaneous/SideDrawer.js
+++ frontend/src/miscellaneous/SideDrawer.js
@@ -1,4 +1,4 @@
-import { Tooltip  , Button, Box, Text, Menu, MenuButton, MenuDivider,MenuItem,MenuList, DrawerOverlay,DrawerHeader,Drawer,DrawerContent, DrawerBody,Input} from '@chakra-ui/react';
+import { Tooltip  , Button, Box, Text, Menu, MenuButton, MenuDivider,MenuItem,MenuList, DrawerOverlay,DrawerHeader,Drawer,DrawerContent, DrawerBody,Input, useToast} from '@chakra-ui/react';
 import React,{useState} from 'react'
 import {BellIcon, ChevronDownIcon} from '@chakra-ui/icons';
 import {Avatar} from '@chakra-ui/avatar'
@@ -6,6 +6,8 @@ import { ChatState } from '../Context/ChatProvider';
 import ProfileModal from './ProfileModal';
 import { useHistory } from 'react-router-dom';
 import {useDisclosure} from '@chakra-ui/hooks'
+import axios from 'axios';
+import ChatLoading from './ChatLoading';
 const SideDrawer = () => {
     const [search,setSearch] = useState("");
     const [searchResult,setSearchResult] = useState([]);
@@ -14,8 +16,39 @@ const SideDrawer = () => {
     const {user} = ChatState();
     const history = useHistory();
     const {isOpen,onOpen,onClose} = useDisclosure();
-    const handleSearch=()=>{
-
+    const toast = useToast();
+    const handleSearch=async ()=>{
+        if(!search){
+            toast({
+                title:"Please enter something in search",
+                status:"warning",
+                duration:5000,
+                isClosable:true,
+                posistion:"top-left"
+            })
+            return;
+        }
+        try {
+            setLoading(true);
+            const config = {
+                headers:{
+                    Authorization:`Bearer  ${user.token}`
+                }
+            }
+            const {data} = await axios.get(`/api/user?search=${search}`,config);
+            setLoading(false);
+            setSearchResult(data);
+        } catch (error) {
+            toast({
+                title:"Error Occured!",
+                description:"Failed to Load the search result",
+                status:"warning",
+                duration:5000,
+                isClosable:true,
+                posistion:"top-left"
+            });
+            return;
+        }
     }
     const logoutHandler = ()=>{
         localStorage.removeItem("userInfo");
@@ -83,6 +116,11 @@ const SideDrawer = () => {
                             }}/>
                     <Button onClick={handleSearch}>Go</Button>
                 </Box>
+                {loading ? (
+                    <ChatLoading/>
+                ):(
+                    <span>results</span>
+                )}
             </DrawerBody>            
             </DrawerContent>
         </Drawer>
