0139a3b1b721238738e1c3795b3df2557b35c1e9
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2024-01-09 00:47:45 +0530

    search chats in group moda

--- frontend/src/miscellaneous/GroupChatModal.js
+++ frontend/src/miscellaneous/GroupChatModal.js
@@ -2,32 +2,97 @@ import { useDisclosure } from '@chakra-ui/hooks'
 import {
     Modal,
     ModalOverlay,
+    // useDisclosure,
     ModalContent,
     ModalHeader,
     ModalFooter,
     ModalBody,
-    ModalCloseButton,Button
+    Input,
+    Box,FormControl,
+    ModalCloseButton,Button, useToast
   } from '@chakra-ui/react'
-import React from 'react'
+import React,{useState} from 'react'
+// import {FormControl} from '@chakra-ui/form-control'
+import { ChatState } from '../Context/ChatProvider'
+import axios from 'axios'
+import UserListItem from '../UserAvatar/UserListItem'
 const GroupChatModal = ({children}) => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [groupChatName,setGroupChatName] = useState();
+    const [selectedUsers,setSelectedUsers] = useState([]);
+    const [searchResult,setSearchResult] = useState([]);
+    const [loading,setLoading] = useState(false);
+    const [search,setSearch] = useState("");
+    const toast = useToast();
+    const {user,chats,setChats} = ChatState();
+    const handleSearch = async (query)=>{
+        setSearch(query);
+        if(!query){
+            return;
+        }
+        try {
+            setLoading(true);
+            const config = {
+                headers:{
+                    Authorization:`Bearer ${user.token}`
+                }
+            };
+            const {data} = await axios.get(`/api/user?search=${search}`,config);
+            console.log(data);
+            setLoading(false);
+            setSearchResult(data);
+        } catch (error) {
+            toast({
+                title:"Error Occured!",
+                description:"Failed to Load the Search results",
+                status:"error",
+                duration:5000,
+                isClosable:true,
+                position:"bottom-left"
+            });
+        }
+    }
+    const handleSubmit = async ()=>{
+
+    }
+    const handleGroup = async ()=>{
+
+    }
+   
     return (
         <>
           <span onClick={onOpen}>{children}</span>
     
-          <Modal isOpen={isOpen} onClose={onClose}>
+          <Modal isOpen={isOpen} onClose={onClose} isCentered>
             <ModalOverlay />
             <ModalContent>
-              <ModalHeader>Modal Title</ModalHeader>
+              <ModalHeader
+                    sx={{display:'flex',justifyContent:'center'}}
+                    fontSize="35px"
+                    fontFamily="Work sans"
+                    d="flex"
+                    justifyContent='center'
+                >Create Group Chat</ModalHeader>
               <ModalCloseButton />
-              <ModalBody>
+              <ModalBody d="flex" flexDir="column" alignItems="center">
+                    <FormControl>
+                        <Input placeholder="Chat Name" mb={3} onChange={(e)=>{setGroupChatName(e.target.value)}}/>
+                    </FormControl>
+                    <FormControl>
+                    <Input placeholder="Add Users eg: Raghav, Neha, Dhruv" mb={1} onChange={(e)=>{handleSearch(e.target.value)}}/>
+                    </FormControl>
+                    {/* selected users */}
+                    {loading?(<div>loading</div>):(
+                        searchResult?.slice(0,4).map((user)=>(
+                            <UserListItem key={user._id} user={user} handleFunction={()=>handleGroup(user)}/>
+                        ))
+                    )}
               </ModalBody>
     
               <ModalFooter>
-                <Button colorScheme='blue' mr={3} onClick={onClose}>
-                  Close
+                <Button colorScheme='blue' onClick={handleSubmit} >
+                  Create Chat
                 </Button>
-                <Button variant='ghost'>Secondary Action</Button>
               </ModalFooter>
             </ModalContent>
           </Modal>
