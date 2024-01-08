a53527ceb615d3b2669b2ebd5d5aedb01d759ad8
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2024-01-09 01:32:52 +0530

    click on a user to start chatting

--- frontend/src/miscellaneous/SingleChat.js
+++ frontend/src/miscellaneous/SingleChat.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import { ChatState } from '../Context/ChatProvider'
+import { Box,Text } from '@chakra-ui/react';
 
 const SingleChat = () => {
     const {user,selectedChat,setSelectedChat} = ChatState();
   return (
-    <div>
-      
-    </div>
+    <>
+        {
+            selectedChat?(
+                <></>
+            ):(
+                <Box sx={{display:'flex'}} d="flex" alignItems="center" justifyContent="center" h="100%">
+                    <Text fontSize="3xl" pb={3} fontFamily="Work sans">
+                        Click on a user to start chatting
+                    </Text>
+                </Box>
+            )
+        }
+    </>
   )
 }
 
