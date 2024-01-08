f13b01298cc78d40263dedf88de8aae1140a4586
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2024-01-09 01:17:24 +0530

    new group using ui

--- frontend/src/miscellaneous/GroupChatModal.js
+++ frontend/src/miscellaneous/GroupChatModal.js
@@ -53,7 +53,40 @@ const GroupChatModal = ({children}) => {
     }
     
     const handleSubmit = async ()=>{
-
+        if(!groupChatName || !selectedUsers){
+            toast({
+                title:"Please fill all the fields",
+                status:"error",
+                duration:5000,
+                isClosable:true,
+                position:"top"
+            });
+            return;
+        }
+        try {
+            const config = {
+                headers:{
+                    Authorization: `Bearer ${user.token}`
+                }
+            };
+            const {data} = await axios.post('api/chat/group',{
+                name:groupChatName,
+                users:JSON.stringify(selectedUsers.map((u)=>(
+                    u._id
+                )))
+            },config)
+            setChats([data,...chats]);
+            onClose();
+            toast({
+                title:"New Group Chat Created!",
+                status:"success",
+                duration:5000,
+                isClosable:true,
+                position:"bottom"
+            });
+        } catch (error) {
+            
+        }
     }
 
     const handleDelete =  (delUser)=>{
