e71bab7d7f02939576071c1643e23bd4ee9266ea
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2024-01-09 23:24:52 +0530

    modal ui complete

--- frontend/src/miscellaneous/UpdateGroupChatModel.js
+++ frontend/src/miscellaneous/UpdateGroupChatModel.js
@@ -7,23 +7,35 @@ import {
     ModalHeader,
     ModalFooter,
     ModalBody,
+    Box,
     ModalCloseButton,
     IconButton,
+    Input,
     useDisclosure,
     useToast,
+    FormControl,
   } from '@chakra-ui/react'
-
 import { ViewIcon } from '@chakra-ui/icons';
 import { ChatState } from '../Context/ChatProvider';
+import UserBadgeItem from '../UserAvatar/UserBadgeItem';
 const UpdateGroupChatModel = ({fetchAgain,setFetchAgain}) => {
     const {isOpen,onOpen,onClose} = useDisclosure();
     const [groupChatName,setGroupChatName] = useState();
     const [search,setSearch]  = useState("");
     const [searchResult,setSearchResult] = useState([]);
     const [loading,isLoading] = useState(false);
-    const [renamLoading,setRenameLoading] = useState(false);
+    const [renameLoading,setRenameLoading] = useState(false);
     const toast = useToast();
     const {selectedChats,setSelectedChats,user} = ChatState();
+    const handleRemove = () => {
+
+    };
+    const handleRename = () => {
+
+    };
+    const handleSearch = () => {
+
+    };
     return (
         <>
 <IconButton sx={{display:'flex'}} icon={<ViewIcon />} onClick={onOpen}>Open Modal</IconButton>
@@ -34,13 +46,37 @@ const UpdateGroupChatModel = ({fetchAgain,setFetchAgain}) => {
     <ModalHeader>{selectedChats.chatName}</ModalHeader>
     <ModalCloseButton />
     <ModalBody>
+        <Box w="100%" sx={{display:'flex'}} flexWrap="wrap" pb={3}>
+            {selectedChats.users.map((u)=>(
+                <UserBadgeItem key={user._id} user={u} handleFunction={()=>handleRemove(u)}/>
+            ))}
+        </Box>
+        <FormControl sx={{display:'flex'}}>
+            <Input
+                placeholder="Chat Name"
+                mb={3}
+                value={groupChatName}
+                onChange={(e)=>setGroupChatName(e.target.value)}
+            />
+            <Button
+                variant="solid"
+                colorScheme='teal'
+                ml={1}
+                isLoading={renameLoading}
+                onClick={handleRename}
+            >Update</Button>
+        </FormControl>
+        <FormControl>
+            <Input placeholder="Add User to group"
+                    mb={1}
+                    onChange={(e)=>handleSearch(e.target.value)} />
+        </FormControl>
     </ModalBody>
 
     <ModalFooter>
-      <Button colorScheme='blue' mr={3} onClick={onClose}>
-        Close
+      <Button colorScheme='red' mr={3} onClick={()=>handleRemove(user)}>
+        Leave Group
       </Button>
-      <Button variant='ghost'>Secondary Action</Button>
     </ModalFooter>
   </ModalContent>
 </Modal>
